d599d3cbdfc71a19f1d3718803f407e389663efb
Leon Farchau <leon@example.com>
2023-11-08 20:09:11 +0100

    Streamline Analyzer Code

--- analyzer.py
+++ analyzer.py
@@ -5,6 +5,7 @@ from matplotlib.animation import FuncAnimation
 import scipy.signal as signal
 from matplotlib.widgets import Button, MultiCursor, RangeSlider
 from matplotlib.ticker import EngFormatter
+from matplotlib import colormaps
 
 # *** load data ***
 emgTimes = np.array([], dtype=np.float32)
@@ -27,7 +28,7 @@ fileName = "dataSamples/pcWade1_s=2000_g=1_c=5.npz"
 fileName = "dataSamples/laptopAtDesktopWithoutPower_s=2000_g=1_c=5.npz"
 #fileName = "dataSamples/pcWade1_s=2000_g=1_c=4.npz"
 
-data = np.load(fileName)
+data = np.load(fileName, allow_pickle=True)
 
 
 if "timeArray" in data:
@@ -53,20 +54,13 @@ data.close()
 ts = emgTimes[-1] - emgTimes[-2]
 fs = 1 / ts
 
-#remove first x seconds
-preSecondsToRemove = 0
-prePointsToRemove = int(preSecondsToRemove * fs)
-
-#remove last x seconds
-postSecondsToRemove = 0
-postPointsToRemove = int(postSecondsToRemove * fs) if postSecondsToRemove > 0 else 1
-
-emgTimes = emgTimes[prePointsToRemove:-postPointsToRemove]
-emgValues = emgValues[prePointsToRemove:-postPointsToRemove]
-
 stabilizeSeconds = 1
 stabilizePoints = int(stabilizeSeconds * fs)
 
+downsampleFactor = 1
+emgTimes = emgTimes[::downsampleFactor]
+emgValues = emgValues[::downsampleFactor]
+fs = fs / downsampleFactor
 
 # *** filter data ***
 def butter_bandstop(lowcut, highcut, fs, order=5):
@@ -128,7 +122,30 @@ def applyHysteresis(values, upperThreshold, lowerThreshold):
             values[i] = values[i - 1]
     return values
 
-# *** plot data ***
+def applyFFTFilter(values, fs, lowerThreshold, upperThreshold, fftsPerSecond, fftWindow, fftSize = 512):
+    samplesPerFFT = int(fs / fftsPerSecond)
+    nperseg = 256
+    noverlap = 256 - samplesPerFFT
+    #hamming, bohmann, barthann window for better frequency resolution
+    f, t, Sxx = signal.spectrogram(emgValues, fs, nfft=fftSize, nperseg=nperseg, \
+                                   noverlap=noverlap, scaling='density', window=fftWindow)
+
+    freqSlice = np.where((f > lowerThreshold) & (f < upperThreshold))
+    f = f[freqSlice]
+    Sxx = Sxx[freqSlice, :][0]
+
+    # Calculate avg FFT for inactivity
+    t_avg = np.where((t > 0.5) & (t < 2.5))
+    Sxx_avg = Sxx[:, t_avg][:,0,:]
+    inactivityFft = np.mean(Sxx_avg, axis=1, keepdims=True)
+    
+    # Use inactivity FFT to filter the actual FFT and normalize it to 0 for inactivity
+    spectrogrammDirectFtt= (Sxx / inactivityFft) - 1
+
+    directFFT = np.mean(spectrogrammDirectFtt, axis=0, keepdims=True)[0]
+    return t, directFFT
+
+# *** plot helper functions ***
 
 def getAxisRange(value, lowerPercentile = 1, upperPercentile = 99, padding = 0.8):
     lower = np.percentile(value, lowerPercentile)
@@ -141,121 +158,134 @@ def createPlot(fileName):
     fig.suptitle(fileName)
     return fig, axes
 
-def drawTimeAx(axes, emgTimes, emgValues, fs):
+def drawTimeDomain(axis, t, y, title, secondY = None, unit="V", signalNames = None, limits = None, useSiUnits = True, **kwargs):
+    plots = []
 
+    for i,signal in enumerate(y):
+        name = signalNames[i] if signalNames is not None else ""
+
+        # plot and set axes limits
+        plots.append(axis.plot(t, signal, label = name, color = colormaps['tab10'](i)))
+
+
+    if secondY is not None:
+        secondYAxis = axis.twinx()
+
+        for i,signal in enumerate(secondY):
+            name = signalNames[i + len(y)] if signalNames is not None else ""
+            
+            plots.append(secondYAxis.plot(t, signal, label = name, color = colormaps['tab10'](i + len(y))))
+            secondYAxis.tick_params(axis='y', labelcolor='tab:orange')
+
+    axis.set_xlim([t[0], t[-1]])
+    axis.set_ylabel('Amplitude [{}]'.format(unit))
+    axis.grid(True)
+    axis.set_title(title)
+
+    #set Limits
+    if limits is not None:
+        if hasattr(limits[0], "__len__"):
+            axis.set_ylim(limits[0])
+            secondYAxis.set_ylim(limits[1])
+        else:
+            axis.set_ylim(limits)
+            secondYAxis.set_ylim(getAxisRange(secondY[0], upperPercentile=99.5, padding=0.05))
+        
+    else:
+        axis.set_ylim(getAxisRange(y[0], upperPercentile=99.5, padding=0.05))
+
+    if useSiUnits:
+        # Label axis as SI units
+        SIformatter = EngFormatter(unit=unit)
+        axis.yaxis.set_major_formatter(SIformatter)
+        if secondY is not None:
+            secondYAxis.yaxis.set_major_formatter(SIformatter)
+
+    if signalNames is not None:
+        axis.legend(ncols = len(signalNames)/2,
+                         loc='upper left', borderaxespad=0.)
+        if secondY is not None:
+            secondYAxis.legend(ncols = len(signalNames)/2,
+                         loc='upper right', borderaxespad=0.)
+
+    return axis, plots
+
+# *** draw plots ***
+
+def drawFFTFilter(axis, y, fs):
+    fftsPerSecond = 50
+    actualWindow = ('dpss', 1.8)
+
+    extT, extFiltered = applyFFTFilter(y, fs, 75, 250, fftsPerSecond, actualWindow, fftSize=512)
+    extFiltered = convolve(extFiltered, 0.3, fftsPerSecond)
+    extFiltered = np.clip(extFiltered, 0, None)
+    extFiltered = np.sqrt(extFiltered)
+    extFilteredHys = extFiltered * applyHysteresis(extFiltered, 0.9, 0.1)
+
+    signalNames = ["FFT Filtered", "FFT Filtered with hysteresis"]
+
+    return drawTimeDomain(axis, extT, [extFiltered, extFilteredHys], "FFT Filtered", drawRMS = False, unit="", \
+                          useSiUnits = False, signalNames = signalNames)
+
+def drawTimeDomainFilter(axis, t, y, fs):
     # Create bandpass-filtered version of signal
-    yLowpass = butter_bandpass_filter(emgValues, 75, 250,fs, 7)
-
-
-    # plot and set axes limits
-    axes[0].plot(emgTimes, emgValues, marker=".", label = "raw", color = 'tab:gray', )
+    yLowpass = butter_bandpass_filter(y, 75, 250,fs, 7)
 
     # Create notch-filtered version of signal
     yNotched = yLowpass
     for fNotch in [50, 100, 150, 200, 250, 300, 350]:
         yNotched = notchData(yNotched, fs, fNotch)
-    axes[1].plot(emgTimes, yNotched, color = 'tab:olive', label = "notch")
 
     # calculate rms
     nothedRMS = calculateRMS(yNotched, fs, windowSize = 0.3)
-    rawRMS = calculateRMS(yLowpass, fs, windowSize = 0.3)
 
     # use hysteresis to only show activity above a certain threshold
     hysterisis = applyHysteresis(nothedRMS, 4e-6, 2.0e-6)
     nothedRMSHyst = nothedRMS * hysterisis
 
-    axRMSFiltered = axes[1].twinx()
-    axRMSFiltered.set_ylabel('RMS [V]')
-    axRMSFiltered.plot(emgTimes, nothedRMS, color = 'tab:blue', label = "rms")
-    axRMSFiltered.plot(emgTimes, nothedRMSHyst, color = 'tab:red', label = "rms_hysteresis")
-    axRMSFiltered.tick_params(axis='y', labelcolor='tab:orange')
-
-    axRMSRaw = axes[0].twinx()
-    axRMSRaw.set_ylabel('RMS [V]')
-    axRMSRaw.plot(emgTimes, rawRMS, color = 'tab:orange', label = "rms")
-    axRMSRaw.tick_params(axis='y', labelcolor='tab:orange')
-    
-
-    for ax in axes[:2]:
-        ax.set_xlim([emgTimes[0], emgTimes[-1]])
-        ax.set_ylabel('Amplitude [V]')
-        ax.grid(True)
-    axes[0].set_title('EMG - raw')
-    axes[1].set_title('EMG - filtered')
-
+   
     #set Limits
-    axes[1].set_ylim(getAxisRange(yNotched[stabilizePoints:], upperPercentile=99.5))
-    axes[0].set_ylim(getAxisRange(emgValues[stabilizePoints:], upperPercentile=98, padding=0.5))
-    axRMSFiltered.set_ylim(getAxisRange(nothedRMS[stabilizePoints:], upperPercentile=98))
-    axRMSRaw.set_ylim(getAxisRange(rawRMS[stabilizePoints:], upperPercentile=98, padding=0.5))
-
+    allY = [yLowpass[stabilizePoints:], yNotched[stabilizePoints:]]
+    limits1 = getAxisRange(allY, upperPercentile=99.5)
+    limits1 = limits1[0], limits1[1] * 1.5
+    limits2 = getAxisRange(nothedRMS[stabilizePoints:], upperPercentile=98, padding=0.5)
+    limits2 = limits2[0], limits2[1] * 1.5
+    signalNames = ["bandpass", "notch", "rms", "rms with hysteresis"]
     
-    # Label axis as SI units
-    SIformatter = EngFormatter(unit='V')
-    axes[0].yaxis.set_major_formatter(SIformatter)
-    axRMSRaw.yaxis.set_major_formatter(SIformatter)
-    axes[1].yaxis.set_major_formatter(SIformatter)
-    axRMSFiltered.yaxis.set_major_formatter(SIformatter)
+    drawTimeDomain(axis, t, [yLowpass, yNotched], "Time-Domain Filtered", secondY=[nothedRMS, nothedRMSHyst], \
+                   unit="V", limits = [limits1, limits2], signalNames = signalNames, useSiUnits = True)
 
     return yNotched
 
-def drawSpectrogramm(axFreq, axDirectFFT, emgValues, fs):
-    windows = ['hamming', 'bohman','barthann', 'blackman', 'blackmanharris', 'flattop', 'hann', 'nuttall', 'parzen', 'triang', 'dpss']
-    actualWindow = windows[3]
+def drawSpectrogramm(axis, y, fs):
     actualWindow = ('dpss', 1.8)
     fftsPerSecond = 50
+    minFreq = 30
+    maxFreq = 500
+
     samplesPerFFT = int(fs / fftsPerSecond)
     nperseg = 256
     noverlap = 256 - samplesPerFFT
-    #hamming, bohmann, barthann window for better frequency resolution
-    f, t, Sxx = signal.spectrogram(emgValues, fs, nfft=512, nperseg=nperseg, noverlap=noverlap, scaling='density', window=actualWindow)
+    f, t, Sxx = signal.spectrogram(y, fs, nfft=512, nperseg=nperseg, noverlap=noverlap, scaling='density', window=actualWindow)
 
-    freqSlice = np.where((f > 30) & (f < 500))
-
+    # Slice out frequencies of interest
+    freqSlice = np.where((f > minFreq) & (f < maxFreq))
     f = f[freqSlice]
     Sxx = Sxx[freqSlice, :][0]
 
-    timeDiff = emgTimes[-1] - t[-1]
-    t = t + timeDiff
-
-    t_avg = np.where((t > 0.5) & (t < 2.5))
-    Sxx_avg = Sxx[:, t_avg][:,0,:]
-    ftt_avg = np.mean(Sxx_avg, axis=1, keepdims=True)
-    spectrogrammDirectFtt= (Sxx / ftt_avg) - 1
-    freqSliceDFft = np.where((f > 75) & (f < 250))
-    spectrogrammDirectFtt = spectrogrammDirectFtt[freqSliceDFft, :][0]
-
-    directFFT = np.mean(spectrogrammDirectFtt, axis=0, keepdims=True)[0]
-    # Create sliding average
-    directFFT = convolve(directFFT, 0.3, fftsPerSecond)
-    directFFT = np.clip(directFFT, 0, None)
-    directFFT = np.sqrt(directFFT)
-    hysterisis = applyHysteresis(directFFT, 0.9, 0.1)
-    windowedFFT = directFFT * hysterisis
-
-
-    # draw direct FFT
-    directFFTPlot = axDirectFFT.plot(t, directFFT, color='tab:blue', label='direct FFT')
-    axDirectFFT.plot(t, hysterisis, color='tab:orange', label='direct FFT hysteresis')
-    axDirectFFT.plot(t, windowedFFT, color='tab:red', label='direct FFT hysteresis windowed')
-
-    axDirectFFT.set_ylabel('Activity')
-
-
     # draw spectrogram
-    colorMesh = axFreq.pcolormesh(t, f, Sxx, shading='gouraud', norm="linear")
+    colorMesh = axis.pcolormesh(t, f, Sxx, shading='gouraud', norm="linear")
 
-    axFreq.set_ylabel('Frequency [Hz]')
-    axFreq.set_xlabel('Time [sec]')
+    axis.set_ylabel('Frequency [Hz]')
+    axis.set_xlabel('Time [sec]')
 
-    fig.colorbar(colorMesh, ax=axFreq, label='Power Spectral \nDensity [V**2/Hz]')
+    fig.colorbar(colorMesh, ax=axis, label='Power Spectral \nDensity [V**2/Hz]')
 
     #set Limits
     percentile = getAxisRange(Sxx, lowerPercentile=1, upperPercentile=90)
     colorMesh.set_clim(0,percentile[1])
-    axDirectFFT.set_ylim(getAxisRange(directFFT, lowerPercentile=10, upperPercentile=100, padding=0.2))
 
-    return f, t, Sxx, colorMesh, directFFTPlot
+    return colorMesh
 
 def drawEvents(axEvent, eventTimes, eventValues):
     numGroups = 1
@@ -279,21 +309,15 @@ def drawEvents(axEvent, eventTimes, eventValues):
                          loc='upper left', borderaxespad=0.)
     axEvent.set_title('Events')
 
-fig, axes  = createPlot(fileName)
+fig, axis  = createPlot(fileName)
 
+drawEvents(axis[0], eventTimes, eventValues)
 
+drawTimeDomain(axis[1], emgTimes, [emgValues], "EMG - raw", unit="V", useSiUnits = True)
+drawTimeDomainFilter(axis[2], emgTimes, emgValues, fs)
+_, fftPlot = drawFFTFilter(axis[-2], emgValues, fs)
 
-downsampleFactor = 1
-emgTimes = emgTimes[::downsampleFactor]
-emgValues = emgValues[::downsampleFactor]
-fs = fs / downsampleFactor
+colorMesh = drawSpectrogramm(axis[-1], emgValues, fs)
 
-drawEvents(axes[0], eventTimes, eventValues)
-
-filtererSignal = drawTimeAx(axes[1:3], emgTimes, emgValues, fs)
-
-colorMesh, directFFTPlot = drawSpectrogramm(axes[-1], axes[-2], emgValues, fs)[-2:]
-
-multi = MultiCursor(None, axes, color='darkred', lw=1)
-
+multi = MultiCursor(None, axis, color='darkred', lw=1)
 plt.show()
